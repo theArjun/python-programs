c4969bcd33e94be2a3a2dfe676d64f74b4c94f47
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-07 12:45:19 +0545

    Assignment of UDEMY for lists and sets

---
+++ sets-lists-assignments.py
@@ -0,0 +1,17 @@
+# creating a list
+country = ["Nepal","India","China"]
+# appending a element to last of a list
+country.append("Bhutan")
+# removing an element from list
+country.remove("China")
+# removing a element from list by indexing
+del(country[1])
+# inserting a element to list by indexing
+country.insert(1,"Maldives")
+
+# creating a set
+country1 = {"Nepal","India","China"}
+# adding element to set
+country1.add("Bhutan")
+# removing element from set
+country1.remove("Nepal")
