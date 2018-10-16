447f506845f719fc2aa70c0f488ba8dd29d5788d
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 01:31:05 +0545

    Introduces about Python Language

---
+++ basics/intro.py
@@ -0,0 +1,8 @@
+print("Python is an interpreted, functional, object oriented high level programming language.")
+print("\nSimple Syntax")
+print("Fast-Inbuilt Libraries")
+
+print("\n Why Python?")
+print("-Easy To Learn\n-Performance\n-Rapid Development\n-Dynamically Typed (For Data Type)\n-Great Community\n-Third Party Libraries")
+
+print("\nVisit here for more Python Libraries : pythonanywhere.com/batteries_included")
