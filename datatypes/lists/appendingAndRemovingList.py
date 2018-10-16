96268db86631d10ab2911151bfe970977c7d19a8
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 03:09:20 +0545

    Added src for appending value and removing value from the list

---
+++ datatypes/lists/appendingAndRemovingList.py
@@ -0,0 +1,14 @@
+lst = [10,20,"Arjun",-10,-30.6]
+print(lst)
+
+#Appending Value
+lst.append(40)
+print(lst)
+
+# Deleting By Value
+lst.remove("Arjun")
+print(lst)
+
+#Deleting By Passing Index
+del(lst[1])
+print(lst)
