78ddcb6d0d48a85ee766cec6db8c3b112f06b9e8
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 02:23:35 +0545

    Added src for strings

---
+++ datatypes/string.py
@@ -0,0 +1,21 @@
+s="You are awesome."
+
+print(s)
+print(type(s))
+# String has data type str
+
+s1 = """ You are the creator of your density."""
+print(s1)
+print(type(s1))
+
+# Indexing of String
+print(s[0])
+print(s[2])
+
+#Repeatition
+print(s*2)
+
+# Length of String
+print(len(s1))
+
+
