664d91eef0bb1a01fc3bda3106226e64c43c4bd2
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 01:56:06 +0545

    Added src for octal numbers

---
+++ datatypes/octalNumbers.py
@@ -0,0 +1,7 @@
+# Octal digits are written in the form of 0O123 i.e. 0O and octal numbers
+
+e= 0O123
+print(e)
+print(type(e))
+
+# Type(e) will show integer as it is already converted into decimal number system
