8f0eda739ebff0fe4562fcab8a94da5c12e81f72
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 01:30:14 +0545

    Illustrates how commenting is done

---
+++ basics/comments.py
@@ -0,0 +1,12 @@
+print("This line gets printed.")
+
+# This is single line commenting in Python
+
+# print("This line doesn't get printed.")
+
+# This is multiple line commenting in Python
+'''print("This paragraph")
+print("of lines")
+print("also don't get printed.")'''
+
+print("A gap of 4 spaces or a Tab is pressed in order to specify indentation. Indentation plays a greater role in Python Scripts. They are not just for prettifying the codes but also helps to specify the block of code as the part of functions, loops or decision control statements.")
