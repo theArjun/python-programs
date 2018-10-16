d1f077fa46dc1d9d2267d1d92dfb81ef170379c4
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 03:59:23 +0545

    Added src for more list methods

---
+++ datatypes/lists/moreListMethods.py
@@ -0,0 +1,18 @@
+lst = [10,20,"Arjun",50.67]
+
+# lst.clear() I still wonder, why I got the error message here.
+print(lst)
+
+print(max(lst)) # Prints the maximum value in the list
+print(min(lst)) # Prints the minimum value in the list
+
+lst.insert(3,99) # Inserts the value 99 at index 3
+print(lst)
+lst.insert(2,"Adhikari") # Inserts the value Adhikari at index 2
+print(lst)
+
+lst.sort() # Sorts the array in ascending order
+print(lst)
+
+lst.sort(reverse=True) # Sorts the list in descending order
+print(lst)
