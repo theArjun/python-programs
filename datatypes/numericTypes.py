0ccb32a274efac87d4083e36078c57ee195a9f95
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 01:47:14 +0545

    Added src for numeric data types

---
+++ datatypes/numericTypes.py
@@ -0,0 +1,28 @@
+a = 10 # This is integer value
+f = 10.23 # This is float value
+
+s = "You are the creator of own future." # This is string
+
+# None Types = An object that doesn't contain any value
+
+# Numeric Types = int,float, and 
+
+# Sequences = str,bytes, bytearray, list, tuple, and range
+
+# Sets doesn't allow duplicates
+
+x=13
+y=100
+z=-66
+print(x,y,z)
+
+i=23.6
+j=34.7
+k=100
+print(i,j,k)
+
+# Type function is used to determine the type of the variable
+print(type(a))
+print(type(f))
+print(type(z))
+print(type(s))
