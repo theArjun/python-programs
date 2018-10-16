da68109920b56a1374ee8a349411ab75fc6ee9ab
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 04:15:15 +0545

    Added src for introduction of tuples

---
+++ datatypes/tuples/tupleIntro.py
@@ -0,0 +1,33 @@
+print("Tuple\n")
+
+print('It is a list which can not be modified. We cant use methods like append(), extend(), insert(), delete(), remove(), clear()')
+
+# Declaring a tuple
+tpl = ()
+print(tpl)
+print(type(tpl))
+
+tpl = (1) # Considers a single integer value
+print(type(tpl))
+
+tpl = (1,) # But now it considers it as a tuple
+print(type(tpl))
+
+# print(tpl[3]) # Returns error message tuple index out of range though it supports indexing
+
+tpl = (1,2,3,4,5)
+
+# Length
+print(len(tpl))
+
+# Repeatition
+print(tpl*3)
+
+tpl1 = tpl*3
+print(tpl1)
+
+print(tpl1.count(4)) # Counts how many times 4 is repeated in tuple.
+
+# Indexing
+print(tpl1[5])
+
