49ed6dcd8a2e0dcf7e8d99cc6d7112f75dea4f4d
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 01:56:37 +0545

    Added print message

--- datatypes/binaryNumbers.py
+++ datatypes/binaryNumbers.py
@@ -1,6 +1,7 @@
 # Binary digits are written in the form of 0B100101 i.e. 0B and binary numbers
 
 e= 0b1010
+print(e)
 print(type(e))
 
 # Type(e) will show integer as it is already converted into decimal number system
