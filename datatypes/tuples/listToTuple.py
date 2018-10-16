b24541025c5ea66092e9569f48e3ca5bf08f8464
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 04:18:35 +0545

    Added src for converting list into tuples

---
+++ datatypes/tuples/listToTuple.py
@@ -0,0 +1,9 @@
+lst = [1,2,"Arjun",20.89]
+ # The following methods are available in Tuple as well as in List.
+ 
+print(type(lst))
+
+# Converts lists into tuples
+tpl = tuple(lst)
+print(tpl)
+print(type(tpl))
