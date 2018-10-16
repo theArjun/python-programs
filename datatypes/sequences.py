ab45bf5dd2307b704d09180f5638c21f70964712
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 02:16:39 +0545

    Describes the sequence data types

---
+++ datatypes/sequences.py
@@ -0,0 +1,5 @@
+print("Sequences Types : ")
+
+print("List - It can store any no of values of objects dynamically and maintains order of them.")
+print("Set - It doesn't allow duplicates (to store any no dynamically)")
+print("Dictionary - It is a map.  It is used for storing keys and values pairs. It has separate set of methods to deal with keys and pairs.")
