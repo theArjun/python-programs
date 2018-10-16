6303a4493e60f60ef2db2f9ba3d8746a2d52802c
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 01:54:43 +0545

    Added src for hexadecimal numbers

---
+++ datatypes/hexadecimalNumers.py
@@ -0,0 +1,7 @@
+# Hexadecimal digits are written in the form of 0XFF i.e. 0X and hexadecimal numbers
+
+e= 0XFF
+print(e)
+print(type(e))
+
+# Type(e) will show integer as it is already converted into decimal number system
