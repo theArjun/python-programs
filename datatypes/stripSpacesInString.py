7888a4a95202d00cc3f6d935605aa907acc6d783
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 02:38:39 +0545

    Added src for stripping the spaces

---
+++ datatypes/stripSpacesInString.py
@@ -0,0 +1,10 @@
+s= "    My name is Arjun Adhikari.     "
+
+# Strips the leading and trailing spaces.
+print(s.strip())
+
+# Strips the leading spaces.
+print(s.lstrip())
+
+# Strips the trailing spaces.
+print(s.rstrip())
