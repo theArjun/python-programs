c78dc723bb988adacb7147c747d60db7ee2121b3
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 09:58:44 +0545

    Added src for input function and type casting

---
+++ printAndInput/inputFunction.py
@@ -0,0 +1,17 @@
+# Input Function
+s=input()
+# Here 's' receives a string value because the default input is string
+print(s)
+ 
+s = input("Enter your name :")
+print(s)
+
+# Type casting
+i = int(input("Enter a integer value : "))
+print(i)
+print(type(i))
+
+st = str(input("Enter a string value : "))
+print(st)
+
+#To enter a string, double quotes are mandatory.
