3dea506bcc3d3ca38b3ddd3698210970fe88b837
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-17 05:45:18 +0545

    Added src for range

---
+++ datatypes/range.py
@@ -0,0 +1,14 @@
+r = range(5) # 5 means the maximum no of elements i.e. index from 0 to 5.
+
+for i in r:
+    print(i)
+
+print("Another Range : ")    
+r = range(1,6) #First Parameter means the starting of range and second parameter means the end of the range.
+for i in r:
+    print(i)
+    
+print("Another Range : ")
+r = range(1,20,3) #First Parameter means the starting of range, second parameter means the end of the range and third parameter means the step operation in range.
+for i in r:
+    print(i)
