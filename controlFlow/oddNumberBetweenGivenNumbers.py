986c87f914a216f705783251f91e12a10a105ea3
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-18 10:08:34 +0545

    Prints odd number between two given numbers

---
+++ controlFlow/oddNumberBetweenGivenNumbers.py
@@ -0,0 +1,6 @@
+beginNumber=int(input("Enter the starting number : "))
+endNumber=int(input("Enter the ending number : "))
+while(beginNumber<=endNumber):
+    if beginNumber%2==1:
+        print(beginNumber)
+        beginNumber+=2
