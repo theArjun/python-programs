b170f7de3f8a4305aecc4c8ba185b56b8f943423
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-18 19:18:30 +0545

    Returns the average of two numbers

---
+++ functions/returningAValue.py
@@ -0,0 +1,8 @@
+# A function is defined which takes two inputs a and b as formal parameters
+def average(a,b):
+    # It returns the average of those numbers by return keyword after arithmetic operation
+    return (a+b)/2
+# It will prompts user for inputs and stores it in a list named num
+num = [int(x) for x in input("Enter two numbers separated by space : ").split()]
+# Then it will print the average by invoking the function inside the print statement
+print("Average of given numbers : ",average(num[0],num[1]))
