9a1434691016df3fa0bed374cad8b258ddf41456
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-19 08:10:07 +0545

    Added src for assigning a function to a variable

---
+++ functions/assignFunctionToAVariable.py
@@ -0,0 +1,11 @@
+# First, we define a function named disp
+def disp():
+    # Then, we assign a variable num to 678
+    num = 678
+    # Then, we print the num
+    print(num)
+
+# To make meet our objective, we need to assign the name of the function to a variable without the parenthesis
+z= disp
+# Then, we can invoke the function anytimie with the name of the variable followed by a pair of parenthesis
+z()
