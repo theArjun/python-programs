36c303c035fe080bf5a3d3911dd83568ba73b0d9
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-19 07:10:42 +0545

    Added src for local and global variables

---
+++ functions/localAndGlobalVariables.py
@@ -0,0 +1,15 @@
+# Global variable
+num = 123
+
+def display():
+    # Without a variable of same name inside the local scope, we can access the global variable
+    y = 678
+    num = 789
+    print(num)
+
+# If we want to print the value of y in global scope, which was defined in local scope, the compiler will throw a error "Name 'y' is not defined"
+print(y)
+
+# But we can access the num variable
+print(num)
+display()
