8ca8a48c7bfed2856f159c4af2a8e760e173e181
Arjun Adhikari <be2017se573@gces.edu.np>
2018-10-26 19:48:16 +0545

    Added documentation 📕

--- stringSwap.py
+++ stringSwap.py
@@ -8,9 +8,13 @@ print("String Two : %s"%(stringTwo))
 # Pre-Defined Method
 # stringOne,stringTwo = stringTwo,stringOne
 
+# First, we concatenate the string in form of list.
 stringOne=list(stringOne)+list(stringTwo)
+# Then we extract the first string from the concatenated list and assign it to string two.
 stringTwo=stringOne[0:len(stringTwo)]
+# Then, we assign the extracted second string to string one.
 stringOne=stringOne[len(stringTwo):len(stringOne)]
+# The string one and string two still contains the list. Below statements convert the list into string.
 stringOne=''.join(stringOne)
 stringTwo=''.join(stringTwo)
 
