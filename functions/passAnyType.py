74dc82be183f8a7ca9b12c6444b951d2dba8991e
thearjun <mailarjunadhikari@gmail.com>
2019-02-01 22:50:06 +0545

    Pass any type : list,type to function

---
+++ functions/passAnyType.py
@@ -0,0 +1,8 @@
+# The function fun() takes list as parameter
+def fun(lst):
+    # Printing each element of list
+    for i in lst:
+        print(i)
+
+# First, a list is passed to function
+fun([1,2,3,4])
