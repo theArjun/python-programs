d042753969e677d1834b2105951052c81a1e55c4
thearjun <mailarjunadhikari@gmail.com>
2019-02-02 01:10:38 +0545

    Added comment

--- sequences/sets/frozenSets.py
+++ sequences/sets/frozenSets.py
@@ -8,5 +8,5 @@ s = {1,2,3,4,5} # This is a set.
 f= frozenset(s) # This is a expression which is supposed not to work. Let's see PVM throws what sort of error message ? 
 f.update([20])
 
-Only Navigate and access the values of frozen set are allowed.
+# Only Navigate and access the values of frozen set are allowed.
 
