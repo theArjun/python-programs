54a0a3164f4f18150f77bb310e29a53fff025a4d
thearjun <mailarjunadhikari@gmail.com>
2019-02-01 23:08:22 +0545

    Demonstrates keyword arguments

---
+++ functions/keywordsArguments.py
@@ -0,0 +1,11 @@
+def average(a, b):
+    print(a)
+    print(b)
+    print((a+b)/2)
+
+
+# Doing this, a is assigned to 10 and b is assigned to 20 according to their position
+average(10, 20)
+
+# This assigns b to 10 and a to 20 irrespective of their position
+average(b=10, a=20)
