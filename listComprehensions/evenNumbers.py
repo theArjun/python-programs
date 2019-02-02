cc829f53c1663a327861cae73bf5495a51ab6d9e
thearjun <mailarjunadhikari@gmail.com>
2019-02-02 13:45:01 +0545

    Prints even number from 1 to 20 using list comprehension

---
+++ listComprehensions/evenNumbers.py
@@ -0,0 +1,9 @@
+# Prints even number from 1 to 20
+
+# Using condition
+result = [x for x in range(1, 21) if x % 2 == 0]
+print(result)
+
+# Using the range function too
+result = [x for x in range(2, 21, 2)] # Initial is 2 and then jumps 2 forward
+print(result)
