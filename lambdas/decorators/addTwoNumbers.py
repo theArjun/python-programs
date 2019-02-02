c75b77452c97840958b2ed7ff79d89621d71bbca
thearjun <mailarjunadhikari@gmail.com>
2019-02-02 11:59:20 +0545

    Calculates sum of two nunmbers using decorators

---
+++ lambdas/decorators/addTwoNumbers.py
@@ -0,0 +1,14 @@
+def decorate_add_number(tempFunction):
+    def wrap_add_number(numOne,numTwo):
+        print("The sum is : ")
+        # Call the pre-defined function with temporary function.
+        result = tempFunction(numOne,numTwo)
+        print(result)
+        print("Keep enjoying mathematical journey")
+    return wrap_add_number
+
+@decorate_add_number
+def add_number(numOne,numTwo):
+    return numOne+numTwo
+
+add_number(5,6)
