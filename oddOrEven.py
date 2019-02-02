3d740ae7d0410a624c6d60e57c861a0d33e7f113
thearjun <mailarjunadhikari@gmail.com>
2019-02-02 10:14:00 +0545

    Check a given number is odd or even using Lambda

---
+++ oddOrEven.py
@@ -0,0 +1,9 @@
+# To check a given number is odd or even using Lambda
+num = int(input("Enter a number : "))
+
+# Syntax >>  lambda someVariable : doThisOne if caseOne else doThisTwo
+result = lambda a: "YES" if a % 2 == 0 else"NO"
+
+print(result(num))
+
+print(result(3))
