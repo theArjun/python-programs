e85f72113de6a866bd05d811c8fffc0baa73b861
thearjun <mailarjunadhikari@gmail.com>
2019-02-02 13:00:27 +0545

    Square of elements in list using Generator Function

---
+++ lambdas/generators/squareOfListElements.py
@@ -0,0 +1,16 @@
+def square_number(lst):
+    for i in lst:
+        yield i**2
+
+lst= [1,2,3,4,5,6]
+
+result = square_number(lst)
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+print(next(result))
+
+# Only six elements are passed, so another calling of generator function will throw StopIteration Exception
+print(next(result))
