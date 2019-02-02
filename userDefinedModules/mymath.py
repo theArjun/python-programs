1e4db082b2415f6b8671a529027091ba9ee4c902
thearjun <mailarjunadhikari@gmail.com>
2019-02-02 13:19:42 +0545

    Creating user defined module

---
+++ userDefinedModules/mymath.py
@@ -0,0 +1,11 @@
+def sum(numOne, numTwo):
+    return numOne + numTwo
+
+def diff(numOne, numTwo):
+    return numOne - numTwo
+
+def mult(numOne, numTwo):
+    return numOne * numTwo
+
+def divi(numOne, numTwo):
+    return numOne / numTwo
---
+++ userDefinedModules/usesModule.py
@@ -0,0 +1,6 @@
+import mymath
+
+print(mymath.sum(20, 10))
+print(mymath.diff(20, 10))
+print(mymath.mult(20, 10))
+print(mymath.divi(20, 10))
