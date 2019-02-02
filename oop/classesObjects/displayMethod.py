998d2111bb2d7906555a7cebd49388fc7f530e36
thearjun <mailarjunadhikari@gmail.com>
2019-02-02 15:28:25 +0545

    Display function to display class attributes

---
+++ oop/classesObjects/displayMethod.py
@@ -0,0 +1,31 @@
+class Programmer:
+    def setName(self, name):
+        self.name = name
+
+    def setSalary(self, salary):
+        self.salary = salary
+
+    def setTechnologies(self, technologies):
+        self.technologies = technologies
+
+    def getName(self):
+        return self.name
+
+    def getSalary(self):
+        return self.salary
+
+    def getTechnologies(self):
+        return self.technologies
+
+    def display(self):
+        print("Name = ", self.name)
+        print("Salary = ", self.salary)
+        print("Technologies = ", self.technologies)
+
+
+p1 = Programmer()
+p1.setName("John")
+p1.setSalary("1234")
+p1.setTechnologies("Java")
+
+p1.display()
