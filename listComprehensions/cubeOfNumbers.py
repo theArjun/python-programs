3de8c42019a96955031b09afcd0eb488be076aee
thearjun <mailarjunadhikari@gmail.com>
2019-02-02 13:39:32 +0545

    Prints cube of numbers in a list using list comprehension

---
+++ listComprehensions/cubeOfNumbers.py
@@ -0,0 +1,12 @@
+# Prints cube of numbers in a list using list comprehension
+
+# Conventional way using normal Python Programming
+result = []
+for i in range(1, 11):
+    result.append(i**3)
+print(result)
+
+# Calculating using List Comprehension
+result = []
+result = [i**3 for i in range(1,11)]
+print(result)
