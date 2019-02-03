acd7430833337fac8e1f77af4030bb911a1edc1c
thearjun <mailarjunadhikari@gmail.com>
2019-02-04 00:11:42 +0545

    Download HTML file

---
+++ networking/downloadingHTML.py
@@ -0,0 +1,14 @@
+import urllib.request
+
+try:
+    url = urllib.request.urlopen("https://www.thearjun.tech/")
+    content = url.read()
+    url.close()
+except urllib.error.HTTPError :
+    print("The web page is not found")
+    exit()
+
+# Write binary
+f = open('thearjun.html','wb')
+f.write(content)
+f.close()
