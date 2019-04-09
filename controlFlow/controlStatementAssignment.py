e2162472585daf4e68f8d77f30648da5395edf63
thearjun <be2017se573@gces.edu.np>
2019-04-10 00:17:36 +0545

    Formatted to PEP8 Standard

--- controlFlow/controlStatementAssignment.py
+++ controlFlow/controlStatementAssignment.py
@@ -1,9 +1,12 @@
-# This programs prints the number till the entered number, skips the multiple of number and doesn't exceed 100.
-num=int(input("Enter a number : "))
-i=0
-while(i<=num):
-    i+=1
-    if i%10==0:continue
-    if i>100:break
+# This programs prints the number till the entered number, skips the
+# of number and doesn't exceed 100.
+num = int(input("Enter a number : "))
+i = 0
+while(i <= num):
+    i += 1
+    if i % 10 == 0:
+        continue
+    if i > 100:
+        break
     print(i)
-    i+=1
+    i += 1
