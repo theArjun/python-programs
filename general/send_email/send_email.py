10eed0f3d592fe47ab54bdf76b84c3a99d6f03ca
thearjun <be2017se573@gces.edu.np>
2019-04-13 18:52:36 +0545

    Send Email using smtplib

---
+++ general/send_email/send_email.py
@@ -0,0 +1,22 @@
+import smtplib, config
+
+
+def send_email(subject, msg):
+
+    try:
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(config.SENDER_EMAIL, config.PASSWORD)
+        message = 'Subject: {} {}'.format(subject, msg)
+        server.sendmail(config.SENDER_EMAIL, config.RECEIPENT_EMAIL, message)
+        server.quit()
+        print("Message sent successfully.")
+    except:
+        print("Message send failed.")
+
+
+subject = 'Hello Man'
+msg = 'How are you? Been a long time.'
+
+send_email(subject, msg)
