f64b22bc454c5fe05261a13cc435ac4962dcf458
thearjun <be2017se573@gces.edu.np>
2019-04-19 22:34:05 +0545

    String or not in Python

---
+++ exceptions/string_or_int.py
@@ -0,0 +1,13 @@
+# Raises exceptoin if a string is entered, not a number as expected.
+try:
+    num = int(input('Enter a number. Otherwise it will raise exception. : '))
+    # Checks if number is instance of integer or not.
+    if not isinstance(num, int):
+        raise Exception()
+except:
+    print("Exception raised. You didn't enter number.")
+# If no exception arises.
+else:
+    print("You've entered a number. You're good.")
+finally:
+    print("Keep practicing python.")
