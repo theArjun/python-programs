7994d2ac01cd38939b1e8659fe1dc381cda1e8e2
thearjun <be2017se573@gces.edu.np>
2019-04-19 07:27:02 +0545

    Formatted to PEP - 8 Standard

--- general/googlesearcher.py
+++ general/googlesearcher.py
@@ -1,9 +1,12 @@
-import requests,sys,webbrowser,bs4 # Beautiful Soup
+import requests
+import sys 
+import webbrowser
+import bs4 # Beautiful Soup
 
 res = requests.get('https://google.com/search?q='+''.join(sys.argv[1:]))
 res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text,"html.parser")
+soup = bs4.BeautifulSoup(res.text, "html.parser")
 linkElements = soup.select('.r a')
-linkToOpen = min(5,len(linkElements))
+linkToOpen = min(5, len(linkElements))
 for i in range(linkToOpen):
-    webbrowser.open('https://google.com'+linkElements[i].get('href'))
+    webbrowser.open('https://google.com'+linkElements[i].get('href'))\
