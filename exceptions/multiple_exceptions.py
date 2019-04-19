cddbfbaa8a73966676f49e5718be6f162023349a
thearjun <be2017se573@gces.edu.np>
2019-04-19 23:22:41 +0545

    Multiple Exceptions in Python

---
+++ exceptions/multiple_exceptions.py
@@ -0,0 +1,10 @@
+# Multiple exceptions
+
+try:
+    my_list = [1, 2, 'Arjun', 6.0, True]
+    print(my_list[10] / 0)
+# Note that parenthesis are necessary for multiple exceptions.
+except(IndexError, ZeroDivisionError):
+    print("Exception occured.")
+finally:
+    print('Wow ! That is new taste of exception.')
