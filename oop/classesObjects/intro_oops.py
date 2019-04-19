abb1304b7c655aef2f6147a07100e20d9404ab81
thearjun <be2017se573@gces.edu.np>
2019-04-20 01:45:26 +0545

    Converted Notebook file to Python file

---
+++ oop/classesObjects/intro_oops.py
@@ -0,0 +1,21 @@
+# Introduction to OOPS
+# - Attributes are declared as variables.
+# - Behaviour are written as methods (functions).
+
+
+class Computer:
+    def config(self):
+        print('i7 8th Gen', '16 GB', '1 TB')
+
+com1 = Computer()
+print(type(com1))
+
+# ID prints the address in memory.
+print(id(com1))
+
+# Computer.config() TypeError: config() missing 1 required positional argument: 'self'
+Computer.config(com1)
+
+#  Commonly used method of calling objects function is :
+com1.config()
+# com1 is passed as parameter behind the scenes in places of self.
