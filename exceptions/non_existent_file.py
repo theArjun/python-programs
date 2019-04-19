984a645389e58082b13be1014d83bfab9802d57c
thearjun <be2017se573@gces.edu.np>
2019-04-19 22:43:51 +0545

    Non Existent Files

---
+++ exceptions/non_existent_file.py
@@ -0,0 +1,10 @@
+# Raises exception if a non-existent file is opened for reading purpose with FileNotFoundError.
+
+try:
+    with open('my_file.txt', 'r') as my_file:
+        data = my_file.read()
+except FileNotFoundError:
+    print("The file doesn't exist.")
+finally:
+    print("You're going really good in Python.")
+
