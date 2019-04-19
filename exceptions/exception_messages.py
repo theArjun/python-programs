b7359bf149260332433b107fbe530bc69fadcdb7
thearjun <be2017se573@gces.edu.np>
2019-04-20 00:29:00 +0545

    Message passing in Exception Handling

---
+++ exceptions/exception_messages.py
@@ -0,0 +1,23 @@
+# Passes Message as attribute to Exceptions
+
+
+class FamilyMemberError(Exception):
+
+    def __init__(self, data):
+        self.data = data
+
+    def __repr__(self):
+        return repr(self.data)
+
+try:
+    no_of_people = int(input('Enter number of people to be expected at family function : '))
+    if no_of_people < 10:
+        raise FamilyMemberError('Too low no of family members for function.')
+    if no_of_people > 5000:
+        raise FamilyMemberError('Too many family members for a small family function.')
+except FamilyMemberError as fme:
+    print(fme.data)
+else:
+    print('Perfect no. for family function.')
+finally:
+    print("Celebrate well.")
