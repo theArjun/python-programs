153d31f41103f1288e7813e3b4a13618d19ed2b4
thearjun <be2017se573@gces.edu.np>
2019-04-20 10:24:59 +0545

    Inheritance in Python

---
+++ oop/inheritance/inheritance_demo.py
@@ -0,0 +1,31 @@
+# Inheritance in Python - Shows Single Inheritance
+
+
+# Super Class
+class A:
+
+    def feature_one(self):
+        print("Feature 1 working.")
+
+    def feature_two(self):
+        print("Feature 2 working.")
+
+
+# This is how inheritance is done in Python.
+# Sub Class
+class B(A):
+
+    def feature_three(self):
+        print("Feature 3 working.")
+
+    def feature_four(self):
+        print("Feature 4 working.")
+
+a_one = A()
+a_one.feature_one()
+
+b_one = B()
+b_one.feature_one()
+b_one.feature_four()
+
+# Multiple Inheritance : class C(P,Q)
