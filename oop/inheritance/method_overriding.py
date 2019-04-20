4857bff3fc2730c22c8de011558564cb76e51d89
thearjun <be2017se573@gces.edu.np>
2019-04-20 17:02:16 +0545

    Method Overriding in Python

---
+++ oop/inheritance/method_overriding.py
@@ -0,0 +1,19 @@
+# Method Overriding
+
+
+class Animal:
+
+    def makesound(self):
+
+        print("Sound of Animal")
+
+
+class Cat(Animal):
+
+    # Overrided Method
+    def makesound(self):
+
+        print("Meow")
+
+cat = Cat()
+cat.makesound()
