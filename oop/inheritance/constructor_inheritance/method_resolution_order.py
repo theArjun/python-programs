e99c7ad77c7c12bd80075fbee3093de3e81e93d1
thearjun <be2017se573@gces.edu.np>
2019-04-20 13:32:26 +0545

    Illustrates Method Order Resolution in Multiple Inheritance

---
+++ oop/inheritance/constructor_inheritance/method_resolution_order.py
@@ -0,0 +1,34 @@
+class A:
+
+    def __init__(self):
+        print("Class A init")
+
+    def feature(self):
+        print("Feature A")
+
+
+class B:
+
+    def __init__(self):
+        print("Class B init")
+
+    def feature(self):
+        print("Feature B")
+
+
+# Due to method order resolution in multiple inheritance, method of left-class
+# is given priority, which also prevents ambiguity. 
+class C(A, B):
+
+    pass
+
+c_one = C()
+c_one.feature()
+
+
+class D(B, A):
+
+    pass
+
+d_one = D()
+d_one.feature()
