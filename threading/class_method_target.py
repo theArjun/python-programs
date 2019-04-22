5beddfae609d5b2c0fae7bb9a644d55cfbd6fd74
thearjun <be2017se573@gces.edu.np>
2019-04-22 09:37:23 +0545

    Class method target

---
+++ threading/class_method_target.py
@@ -0,0 +1,12 @@
+from threading import *
+
+
+class Sanskaar:
+
+    def greet(self, name='Arjun'):
+        print("Hello {}".format(name))
+
+
+greeter = Sanskaar()
+thread_one = Thread(target=greeter.greet)
+thread_one.start()
