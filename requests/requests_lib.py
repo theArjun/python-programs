a4330f86e348c5859f8054dc17c552521abeaaf9
thearjun <be2017se573@gces.edu.np>
2019-04-23 10:20:57 +0545

    Intro to requests library

---
+++ requests/requests_lib.py
@@ -0,0 +1,16 @@
+import requests
+
+
+url = 'https://thearjun.tech'
+res = requests.get(url)
+
+print(res.ok)
+print(res.status_code)
+print(res.headers)
+
+# Returns the header items
+print(res.headers.items())
+
+# Formatting response headers
+for key, value in res.headers.items():
+    print(key, '    :    ', value)
