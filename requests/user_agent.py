6541fef56c45fa174b1d62c320669d3cab0cb9ce
thearjun <be2017se573@gces.edu.np>
2019-04-23 10:12:09 +0545

    Creates fake user agent

---
+++ requests/user_agent.py
@@ -0,0 +1,9 @@
+# Creating fake User Agent
+# User agent is a software that sends requests to browser in behalf of user.
+# Browsers like Google chrome, Mozilla firefox are  user agents.
+
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+header = {'user_agent': ua.chrome}
+print(ua.chrome)
