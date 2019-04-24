d36cb96ec92ab16ebe4ea1387c3d2638b72440c1
thearjun <be2017se573@gces.edu.np>
2019-04-24 15:09:05 +0545

    Remove all vowels in a word

---
+++ comprehensions/remove_all_vowels.py
@@ -0,0 +1,9 @@
+# Remove all of the vowels in a string
+
+vowels = ['a', 'b', 'c', 'd', 'e']
+
+test_string = input()
+my_str = [char for char in test_string if char.lower() not in vowels]
+
+# To print the string, rather than list.
+print(''.join(my_str))
