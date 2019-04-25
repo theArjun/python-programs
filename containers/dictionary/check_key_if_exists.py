0b201cc980822e49c62f3b844b53ddd8df940466
thearjun <be2017se573@gces.edu.np>
2019-04-25 23:40:08 +0545

    Check key if exists

---
+++ containers/dictionary/check_key_if_exists.py
@@ -0,0 +1,10 @@
+#  Write a Python script to check if a given key already exists in a dictionary
+
+sample_dict = {'nepal': 'asia', 'congo': 'africa', 'estonia': 'europe'}
+
+sample_country = input()
+
+if sample_country in sample_dict:
+    print("Already in Dictionary")
+else:
+    print("Not in Dictionary")
