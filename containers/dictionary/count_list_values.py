5b0fa26d506c68f40e4988d89e583826334256e2
thearjun <be2017se573@gces.edu.np>
2019-04-25 23:38:45 +0545

    Count list values in dictionary

---
+++ containers/dictionary/count_list_values.py
@@ -0,0 +1,12 @@
+# Write a Python program to count number of items in a dictionary value that
+# is a list.
+
+sample_dict = {'Alex': ['subj1', 'subj2', 'subj3'],
+               'David': ['subj1', 'subj2'], 'Tom': 'sub4'}
+
+count = 0
+for value in sample_dict.values():
+    if isinstance(value, list):
+        count += 1
+
+print(count)
