01b6ddd81fedb457aba4144a430aafdb3430751b
thearjun <be2017se573@gces.edu.np>
2019-04-25 10:09:36 +0545

    Multiply by ten

---
+++ lambdas/map/multiply_by_ten.py
@@ -0,0 +1,8 @@
+# If we want to change whole chunk of data based on specific operation, we use
+# map.
+
+lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# It returns the map object, so converting it into iterable is necessary.
+result = map(lambda n: n*10, lst)
+print(list(result))
