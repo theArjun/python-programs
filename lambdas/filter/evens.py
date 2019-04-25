c2fe9d4dc6f49ec2809a0ba15cb38709fd1a3f36
thearjun <be2017se573@gces.edu.np>
2019-04-25 10:04:29 +0545

    Added info

--- lambdas/filter/evens.py
+++ lambdas/filter/evens.py
@@ -2,6 +2,8 @@
 # Function and itearble
 
 
+# Filter are used to filter the iterables based on specific condition.
+
 # We can use defined function or lambda both.
 nums = [1, 2, 3, 4, 5, 6, 7]
 
@@ -9,7 +11,8 @@ nums = [1, 2, 3, 4, 5, 6, 7]
 def is_even(n):
     return n % 2 == 0
 
-
+# First it return the filter object, so we need to convert it into list.
+# We can also convert it in set, tuple etc.
 evens = list(filter(is_even, nums))
 
 # By using lamdba.
