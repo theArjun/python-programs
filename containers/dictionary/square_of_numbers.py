6e8d957de7fe89358913b69d9c86999816a4922e
thearjun <be2017se573@gces.edu.np>
2019-04-25 23:36:11 +0545

    Square of a number

---
+++ containers/dictionary/square_of_numbers.py
@@ -0,0 +1,6 @@
+# Write a Python script to generate and print a dictionary that contains a
+# number (between 1 and n) in the form (x, x*x).
+
+num = int(input)
+my_dict = {num: num*num for num in range(1, num+1)}
+print(my_dict)
