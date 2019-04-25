ce04f27d0eb33b53c57b584d90c24261f4a65a3f
thearjun <be2017se573@gces.edu.np>
2019-04-25 23:39:35 +0545

    Dictionary from object field

---
+++ containers/dictionary/get_dictionary_from_object_field.py
@@ -0,0 +1,18 @@
+# Write a Python program to get a dictionary from an object's fields.
+
+
+class Student:
+
+    def __init__(self, name, roll, grade):
+
+        self.name = name
+        self.roll = roll
+        self.grade = grade
+
+    def convert_to_dict(self):
+
+        return {'name': self.name, 'roll': self.roll, 'grade': self.grade}
+
+
+s_one = Student('Arjun', 5, 'Bachelors')
+print(s_one.convert_to_dict())
