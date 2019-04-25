6547de42039e9e09aaa7d01219db920238461dd6
thearjun <be2017se573@gces.edu.np>
2019-04-25 22:59:28 +0545

    Added print code for split to zero

--- containers/strings/max_split.py
+++ containers/strings/max_split.py
@@ -7,9 +7,19 @@ print(my_intro.split())
 # same as
 print(my_intro.split(' '))
 
+print("When splitted by 0 : ", end=" : ")
+print(my_intro.split(' ', 0))
+
+print("When splitted by 1 : ", end=" : ")
 print(my_intro.split(' ', 1))
+
+print("When splitted by 2 : ", end=" : ")
 print(my_intro.split(' ', 2))
+
+print("When splitted by 3 : ", end=" : ")
 print(my_intro.split(' ', 3))
+
+print("When splitted by 4 : ", end=" : ")
 print(my_intro.split(' ', 4))
 
 # When parameter in split() method reaches length of words in string,
