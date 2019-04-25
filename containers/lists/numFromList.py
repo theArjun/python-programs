f44bfe739d8d66ca738b6832c4ae68ecb46c1506
thearjun <be2017se573@gces.edu.np>
2019-04-25 12:03:52 +0545

    Added new way of deleting index

--- containers/lists/numFromList.py
+++ containers/lists/numFromList.py
@@ -1,16 +1,28 @@
 # Credit : Abiral Bhattarai
 # Title : Delete a element of order x from List in Python
 
-a = int(input("How many number you want to enter?"))
-numlist = list()  # Entered array
-latelist = list()
-for i in range(a):
-    x = int(input("Enter the number="))
-    numlist.append(int(x))
-print("Entered numbers array")
-print(numlist)
-b = int(input("Which order array you want to delete?"))
-for i in range(a):
-    if b != i:
-        latelist.append(int(numlist[i]))
-print(latelist)
+# a = int(input("How many number you want to enter?"))
+# numlist = list()  # Entered array
+# latelist = list()
+# for i in range(a):
+#     x = int(input("Enter the number="))
+#     numlist.append(int(x))
+# print("Entered numbers array")
+# print(numlist)
+# b = int(input("Which order array you want to delete?"))
+# for i in range(a):
+#     if b != i:
+#         latelist.append(int(numlist[i]))
+# print(latelist)
+
+# Author : Arjun Adhikari
+a = int(input("How many number you want to enter ?"))
+lst = input().split()[:a]
+# Also can be done like this.
+# lst = list(map(lambda x: int(x), input().split()[:a]))
+lst = list(map(lambda x: int(x), lst))
+print(lst)
+
+b = int(input("Which order array you want to delete ?"))
+del(lst[b-1])
+print(lst)
