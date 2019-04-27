cdca23c3785508174637f7795382dbcd4e1248f6
thearjun <be2017se573@gces.edu.np>
2019-04-27 11:36:51 +0545

    Improved input data, added exceptions

--- selenium/linkedin_wihdraw_requests.py
+++ selenium/linkedin_wihdraw_requests.py
@@ -1,20 +1,21 @@
 # Cancel all the LinkedIn sent invitations
 
-
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
-# For waiting page to fully load
-from selenium.webdriver.support.ui import WebDriverWait
 import time
+# For entering password from console
+import getpass
+# For quitting app if login fails.
+import sys
 
-driver = webdriver.Firefox(executable_path="your_path")
 
-lst = []
-
+driver = webdriver.Firefox(executable_path="C:\selenium\geckodriver.exe")
 driver.get("https://www.linkedin.com")
 
-
 try:
+    # Linkedin serves two login page : One Clean UI and other old design.
+    # This try block is for Clean UI.
     sign_in = driver.find_element_by_class_name("nav__button-secondary")
     print('Sign In Link Display : ', sign_in.is_displayed())
     sign_in.click()
@@ -23,35 +24,63 @@ try:
     password = driver.find_element_by_name("session_password")
     login = driver.find_element_by_css_selector(".btn__primary--large")
 
-    # print("In Clean UI Page : ")
-    # print("email Display : ", email.is_displayed())
-    # print("Password Display : ", password.is_displayed())
-    # print("Login Display : ", login.is_displayed())
+    print("In Clean UI Page : ")
+    print("email Display : ", email.is_displayed())
+    print("Password Display : ", password.is_displayed())
+    print("Login Display : ", login.is_displayed())
 
-except:
+except NoSuchElementException:
 
+    # This block is for old designed UI.
     email = driver.find_element_by_id("login-email")
     password = driver.find_element_by_id("login-password")
     login = driver.find_element_by_id("login-submit")
 
-    # print("In Old Designed Page : ")
-    # print("email Display : ", email.is_displayed())
-    # print("Password Display : ", password.is_displayed())
-    # print("Login Display : ", login.is_displayed())
+    print("In Old Designed Page : ")
+    print("email Display : ", email.is_displayed())
+    print("Password Display : ", password.is_displayed())
+    print("Login Display : ", login.is_displayed())
 
-email.send_keys("mailarjunadhikari@gmail.com")
-password.send_keys("********")
+em = input("Enter Email : ").rstrip()
+email.send_keys(em)
+pwd = getpass.getpass("Enter password : ")
+password.send_keys(pwd)
 login.click()
 
-# WebDriverWait(driver, 10)
+login_status = False
+
+# Checking if user has login successfully or not.
+try:
+    messaging_check = driver.find_element_by_id("messaging-nav-item")
+except NoSuchElementException:
+    print("\nLogin failed.")
+    sys.exit(0)
+
+
+print("Successfully Logged in.")
 
 driver.get("https://www.linkedin.com/mynetwork/invitation-manager/sent/")
 
+limit = 0
+
+while not limit > 0:
+    limit = int(input("\nLeave empty if you want to withdraw all requests.\n"
+                      "Enter numeric value starting from 0 to your choice\n"
+                      "Enter how many withdraw you want : "))
+
+withdraw_count = 0
+
 while True:
     try:
-        withdraw = driver.find_element_by_class_name(
-            "invitation-card__action-btn")
-        withdraw.click()
-        time.sleep(2)
-    except:
+        if withdraw_count <= limit:
+            withdraw = driver.find_element_by_class_name(
+                "invitation-card__action-btn")
+            withdraw.click()
+            time.sleep(2)
+            withdraw_count += 1
+        else:
+            print("Request withdraw completed.")
+            break
+    except NoSuchElementException:
+        print("No request to withdraw.")
         break
