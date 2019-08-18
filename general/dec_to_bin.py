3805bd045b9d6f07b3574749966771ea18ff39da
thearjun <mailarjunadhikari@gmail.com>
2019-08-18 19:01:07 +0545

    Decimal to Binary

--- general/dec_to_bin.py
+++ general/dec_to_bin.py
@@ -2,11 +2,6 @@ def dec_to_bin(num):
 
     ans = []
     while num > 0:
-
-        rem = num % 2
+        ans.append(str(num % 2))
         num = num // 2
-        ans.append(rem)
-
-    return ''.join(list(map(str, ans))[::-1])
-
-print(dec_to_bin(int(input())))
+    return int(''.join(ans)[::-1])
