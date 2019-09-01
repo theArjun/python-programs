72d3ed856cf7d80a36df2df610bffe10983e2ed1
thearjun <mailarjunadhikari@gmail.com>
2019-09-01 23:17:34 +0545

    SIngle Line Docstring in Python

---
+++ docstrings/foo.py
@@ -0,0 +1,14 @@
+"""
+        Title : Docstrings in Python
+        Description : Illustrates how docstrings acts in Python.
+        Referencing URL : None
+        Date : 2019-09-01
+        Written by : ARJUN ADHIKARI
+"""
+
+def foo():
+
+    """ This is the string literal used as a single line docstring. """
+
+# The __doc__ magic method will return the docstring of the function.
+print(foo.__doc__)
